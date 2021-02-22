629387e2fa6d04193fb85cc11d13f7096ae41c50
ayu129 <ayu.daddybear.29@gmail.com>
2021-02-22 17:07:12 +0900

    Initial Commit

---
+++ soreseikai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  soreseikai
+//
+//  Created by 伊藤愛結 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
